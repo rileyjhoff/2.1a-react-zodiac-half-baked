5775bea462affcf3f7e6965f4cd3da1d9bca4073
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-25 14:14:10 -0700

    fixed path issue with zodiac images

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -4,7 +4,7 @@ export default function ZodiacCard({ name, dates }) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
   return (
     <div className='zodiac-card'>
-      <img src={`${name}.png`} />
+      <img src={`images/${name}.png`} />
       <p className='name'>{name}</p>
       <p>{dates}</p>
     </div>
