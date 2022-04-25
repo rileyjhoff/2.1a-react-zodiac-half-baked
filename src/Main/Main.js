74094420ddfb116d11dcf83659a00752fbc02f69
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-25 15:47:16 -0700

    beginning phases of adding slider functionality

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -10,15 +10,17 @@ export default function Main({ zodiacs }) {
       {/* get the zodiac array from props */}
       {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
       {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
-      {
-        zodiacs.map(zodiac => 
+      <div className='slider-button'></div>
+      <div className='container'>
+        {zodiacs.map(zodiac => 
           <ZodiacCard 
             key={zodiac.name}
             name={zodiac.name}
             dates={zodiac.dates}
           />
-        )
-      }
+        )}
+      </div>
+      <div className='slider-button'></div>
     </main>
   );
 }
